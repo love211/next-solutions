ca96011fee24db37893ecac8998799d1bb0c7888
love <lovewebdevtech123@gmail.com>
2024-07-24 15:31:23 +0530

    Digital cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import DigitalCard1 from "./digitalCards/DigitalCard1";
+import DigitalCard2 from "./digitalCards/DigitalCard2";
 import "./language/i18n";
 import RoutesProvider from "./routes";
 
 function App() {
-  return <RoutesProvider />;
+  return <DigitalCard2/>;
 }
 
 export default App;
---
+++ src/digitalCards/DigitalCard2.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DigitalCard2 = () => {
+  return (
+    <div className="flex justify-center items-center h-screen text-white text-center border-[#545454]">
+      <div className="mock-up-2  overflow-hidden text-center  border-[#545454]   rounded-[1.25rem] flex flex-col outline outline-[5px] outline-black ">
+      <div className="h-full flex flex-col justify-end pb-8">
+            <img
+              src="https://i.pinimg.com/564x/c6/b4/ec/c6b4ec629a470bd2eb1d3328226495cc.jpg"
+              alt="profile"
+              className=" w-40 h-40  rounded-full border-[4px]"
+            />
+           
+          </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default DigitalCard2;
