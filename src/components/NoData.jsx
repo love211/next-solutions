72db5b570ca9c656f83660b0027956708043201f
love <lovedevtech123@gmail.com>
2024-08-01 18:58:07 +0530

    404 iage change

--- src/components/NoData.jsx
+++ src/components/NoData.jsx
@@ -1,11 +1,11 @@
-let awsURL = "";
 import image404 from "../assets/404.jpg";
 const NoData = () => {
-  console.log("404u");
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-fit h-fit min-w-fit flex flex-col items-center gap-4">
-        <img src={image404} alt="" className="w-20 h-24" />
+        <div>
+          <img src={image404} alt="" className="object-fill" />
+        </div>
         <p className="text-primary text-xl text-pretty">
           No Templates are created yet..! please create one.
         </p>
